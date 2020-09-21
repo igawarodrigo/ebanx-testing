84a7936b8874009ad43514b95b7212e698738383
Rodrigo Igawa <rodrigoigawa@Rodrigos-MacBook-Pro-2.local>
2020-09-21 18:18:22 -0300

    Uses static import

--- src/main/java/com/ebanx/homeassigment/assignment/AssignmentApplication.java
+++ src/main/java/com/ebanx/homeassigment/assignment/AssignmentApplication.java
@@ -1,10 +1,11 @@
 package com.ebanx.homeassigment.assignment;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.READ_ENUMS_USING_TO_STRING;
 
 @SpringBootApplication
 public class AssignmentApplication {
@@ -16,7 +17,7 @@ public class AssignmentApplication {
 	@Bean
     public ObjectMapper objectMapper () {
 	    ObjectMapper objectMapper = new ObjectMapper();
-	    objectMapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
+	    objectMapper.enable(READ_ENUMS_USING_TO_STRING);
 	    return objectMapper;
     }
 }
